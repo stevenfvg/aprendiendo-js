22ca2d730f3caaaf2c449c9bffe3763c9e443645
stevenfvg <stevenvalladaresgiron@gmail.com>
2023-11-06 23:00:28 -0300

    chore: :memo: basic example of variables added

---
+++ examples/variables.js
@@ -0,0 +1,16 @@
+// Existen 3 formas de crear variables en JavaScript, en nuevas versiones solo se utilizan 2:
+// Declaración de variables con let
+let variableLet = "Soy una variable declarada con let";
+console.log(variableLet);
+
+// Reasignación de variable let
+variableLet = "He sido reasignada con let";
+console.log(variableLet);
+
+// Declaración de variables con const
+const variableConst = "Soy una variable declarada con const";
+console.log(variableConst);
+
+// Intento de reasignación de variable const (esto generará un error)
+// variableConst = "No puedo ser reasignada con const";
+// Javascript es un lenguaje de tipo Dinamico, no se especifican tipos de datos.
