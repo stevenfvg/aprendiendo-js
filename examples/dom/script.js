c1658f21979891f0511e559c6dccfaee3fe6d160
stevenfvg <stevenvalladaresgiron@gmail.com>
2023-11-19 21:07:59 -0300

    docs: :heavy_plus_sign: changes are added to the dom example

--- examples/dom/script.js
+++ examples/dom/script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // Acceder al dominio del sitio web
+  const dominio = document.domain;
+  console.log('Dominio: ', dominio);
+
+  // Acceder al total de enlaces en el DOM
+  const enlaces = document.links.length;
+  console.log('Enlaces: ', enlaces);
+
+  // Acceder al total de imágenes en el DOM.
+  const imagenes = document.images.length;
+  console.log('Imágenes: ', imagenes);
+
   // Acceder a un elemento por su ID
   const titulo = document.getElementById('contenido');
   titulo.style.backgroundColor = '#e0e0e0';
@@ -13,9 +25,22 @@ document.addEventListener('DOMContentLoaded', () => {
   const primerParrafo = document.querySelector('.parrafo');
   primerParrafo.style.color = 'red';
 
+  // Modificar texto con querySelector
+  const nuevoEncabezado = 'Mi Página Web';
+  document.querySelector('h1').textContent = nuevoEncabezado;
+
   // Usar querySelectorAll para seleccionar todos los párrafos
   const todosLosParrafos = document.querySelectorAll('.parrafo');
-  todosLosParrafos.forEach(parrafo => {
+  todosLosParrafos.forEach((parrafo) => {
     parrafo.style.fontSize = '18px';
   });
+
+  // Generar HTML con JavaScript
+  const nuevoTexto = document.createElement('span');
+  nuevoTexto.innerHTML = `<em>Abre la consola del navegador con (Ctrl + Shift + I).</em>`;
+  nuevoTexto.style.fontSize = 'small';
+
+  // Agregar el nuevo texto al DOM
+  const contenido = document.getElementById('contenido');
+  contenido.appendChild(nuevoTexto);
 });
