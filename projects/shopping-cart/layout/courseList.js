7d5e3a489bbc4bd4454b6eccd01c17fd13c644e1
stevenfvg <stevenvalladaresgiron@gmail.com>
2024-03-09 22:54:45 -0400

    style(project): grid design is added to the course container

--- projects/shopping-cart/layout/courseList.js
+++ projects/shopping-cart/layout/courseList.js
@@ -2,9 +2,10 @@ export default function courseList() {
     return `
         <div id="lista-cursos" class="container">
             <h1 id="encabezado" class="encabezado">Cursos En Línea</h1>
-            <div class="row">
-                <div class="four columns">
-                    
+            <div class="grid-container">
+                <div class="grid-item">
+                    <div class="card">
+                    </div>
                 </div>
             </div>          
         </div>  
